36bf18ff590250c4b0a4d5755a5b6b87eda6de32
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-10-20 00:06:23 +0100

    Add additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,21 @@
+import { Box, Flex, Card } from '@radix-ui/themes'
 import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>Loading...</div>
+    <Box className='max-w-xl'>
+      <Skeleton width="20rem" height='1.2rem' />
+
+      <Flex gap='3' my='2'>
+        <Skeleton width="5rem" height='1.6rem' />
+        <Skeleton width="8rem" height='1.6rem' />
+      </Flex>
+      <Card className='prose' mt='4'>
+        <Skeleton count={5} />
+      </Card>
+    </Box>
   )
 }
 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,17 @@
 'use client'
 
-import React, { useState } from 'react'
-import { Button, Callout } from '@radix-ui/themes'
-import SimpleMDE from 'react-simplemde-editor'
-import 'easymde/dist/easymde.min.css'
-import { useForm, Controller } from 'react-hook-form'
-import { TextField } from '@radix-ui/themes'
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { createIssueSchema } from '@/app/validationSchemas'
-import { z } from 'zod'
 import ErrorMessage from '@/app/components/ErrorMessage'
 import Spinner from '@/app/components/Spinner'
+import { createIssueSchema } from '@/app/validationSchemas'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Callout, TextField } from '@radix-ui/themes'
+import axios from 'axios'
+import 'easymde/dist/easymde.min.css'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import SimpleMDE from 'react-simplemde-editor'
+import { z } from 'zod'
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
