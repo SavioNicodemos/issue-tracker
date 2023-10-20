e8c6a80eebeb158e78e1bca0df29a72b27073488
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-10-20 19:56:35 +0100

    Request to Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -38,7 +38,9 @@ const IssueForm = ({ issue }: Props) => {
   async function handleCreateIssue(data: IssueFormData) {
     try {
       setIsSubmitting(true);
-      await axios.post('/api/issues', data);
+
+      if (issue) await axios.patch(`/api/issues/${issue.id}`, data);
+      else await axios.post('/api/issues', data);
 
       router.push('/issues');
     } catch (error) {
@@ -73,7 +75,10 @@ const IssueForm = ({ issue }: Props) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-        <Button disabled={isSubmitting}> Submit New Issue {isSubmitting && <Spinner />}</Button>
+        <Button disabled={isSubmitting}>
+          {issue ? 'Update Issue' : 'Submit New Issue'}&nbsp;
+          {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div >
   )
