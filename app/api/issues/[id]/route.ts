0fee9f988db0affb231105f20a02914b8a784ae5
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-10-21 21:55:03 +0100

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,8 +1,10 @@
 import authOptions from '@/app/auth/authOptions';
-import { createIssueSchema } from '@/app/validationSchemas';
+import { patchIssueSchema } from '@/app/validationSchemas';
 import prisma from '@/prisma/client';
+import { User } from '@prisma/client';
 import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
+import z from 'zod';
 
 type Props = {
   params: {
@@ -10,16 +12,29 @@ type Props = {
   };
 };
 
+type PatchBody = z.infer<typeof patchIssueSchema>;
+
 export async function PATCH(request: NextRequest, { params }: Props) {
   const session = await getServerSession(authOptions);
   if (!session)
     return NextResponse.json({ message: 'Unauthorized.' }, { status: 401 });
 
   const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
+  const { assignedToUserId, title, description } = body as PatchBody;
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  let user: User | null = null;
+  if (assignedToUserId) {
+    user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+
+    if (!user)
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: Number(params.id) },
@@ -30,8 +45,9 @@ export async function PATCH(request: NextRequest, { params }: Props) {
   const updatedIssue = await prisma.issue.update({
     where: { id: Number(params.id) },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId: user?.id ?? null,
     },
   });
 
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -5,5 +5,27 @@ export const createIssueSchema = z.object({
     .string()
     .min(1, 'The title field is required.')
     .max(255, 'The text should be less than 255 characters.'),
-  description: z.string().min(1, 'The description field is required.'),
+  description: z
+    .string()
+    .min(1, 'The description field is required.')
+    .max(60000, 'The text should be less than 60.000 characters.'),
+});
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, 'The title field is required.')
+    .max(255, 'The text should be less than 255 characters.')
+    .optional(),
+  description: z
+    .string()
+    .min(1, 'The description field is required.')
+    .max(60000, 'The text should be less than 60.000 characters.')
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, 'The assignedToUserId field is required.')
+    .max(255, 'The text should be less than 255 characters.')
+    .optional()
+    .nullable(),
 });
