eb29c8bf62c51aecec8490ccb00037a6fd20f3db
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-10-23 02:49:45 +0100

    Add dynamic routes for dashboard and get users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany({
